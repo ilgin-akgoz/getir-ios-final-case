685559e54f08111d9adf161fcc200ba6e416dcc0
ilgin-akgoz <82042228+ilgin-akgoz@users.noreply.github.com>
2024-04-15 14:35:23 +0300

    Create the project and remove storyboard

---
+++ GetirShoppingApp/GetirShoppingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetirShoppingApp
+//
+//  Created by Ilgın Akgöz on 15.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
